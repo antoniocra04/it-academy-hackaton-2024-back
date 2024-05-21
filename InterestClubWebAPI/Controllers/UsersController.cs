5cb5ba112a2fdde33786c77bc47f6421a3a96a4d
dr_verk <matvejebel8@gmail.com>
2024-05-22 00:01:00 +0700

    1 версия

--- InterestClubWebAPI/Controllers/UsersController.cs
+++ InterestClubWebAPI/Controllers/UsersController.cs
@@ -1,12 +1,32 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using InterestClubWebAPI.Context;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Data.Entity;
+using System.Xml.Linq;
 
 namespace InterestClubWebAPI.Controllers
 {
     public class UsersController : Controller
     {
+        private readonly ApplicationContext dbContext;
         public IActionResult Index()
         {
             return View();
+        }
+        [HttpPost("singUp")]
+        public IActionResult SingUp(string login, string password, string confirmation_pasword)
+        {
+            if (string.IsNullOrEmpty(login) || string.IsNullOrEmpty(password) || string.IsNullOrEmpty(confirmation_pasword))// защита от пустых строк
+                return BadRequest();
+           //var user = dbContext.Users.Where(p => p.Login == login).ToList();
+            if (!dbContext.Users.Any(p => p.Login == login ) && confirmation_pasword == password )// проверка свободного логина и соответствия поролей
+            {
+                dbContext.Users.Add(new Models.User { Login=login, Password= password});
+                dbContext.SaveChanges();
+                return Ok();
+            }
+            else
+                return BadRequest();
         }
     }
 }
--- InterestClubWebAPI/Models/User.cs
+++ InterestClubWebAPI/Models/User.cs
@@ -5,6 +5,10 @@ namespace InterestClubWebAPI.Models
     {
         public Guid Id { get; set; }
 
+        public string Login { get; set; }
+        
+        public string Password { get; set; }
+
         public string Name { get; set; }
 
         public string Surname { get; set; }
