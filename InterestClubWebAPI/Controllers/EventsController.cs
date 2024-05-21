17229a240e8f6cdfde5a42fd977607e1dc72dfc8
ShadowRec <93128005+ShadowRec@users.noreply.github.com>
2024-05-22 00:45:38 +0700

    Added DelEvent
    *добавил* роут удаления ивента

--- InterestClubWebAPI/Controllers/EventsController.cs
+++ InterestClubWebAPI/Controllers/EventsController.cs
@@ -1,12 +1,45 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using InterestClubWebAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using InterestClubWebAPI.Context;
 
 namespace InterestClubWebAPI.Controllers
 {
     public class EventsController : Controller
     {
+        private readonly ApplicationContext dbContext;
+        IWebHostEnvironment _appEnvironment;
         public IActionResult Index()
         {
             return View();
         }
+       
+
+        [HttpPost("DelEvent")]
+        public async Task<ActionResult> DelEvent(Event events) 
+        {
+
+            if ((dbContext.Events.Find(events.Id) == null))
+            {
+                return BadRequest("Event is not Found!");
+            }
+            else
+            {
+                dbContext.Events.Remove(events);
+                dbContext.SaveChanges();
+                return Ok();
+            }
+           
+
+        }
+        
+        public EventsController(ApplicationContext dbContext, IWebHostEnvironment appEnvironment)
+        {
+            dbContext = dbContext;
+            _appEnvironment = appEnvironment;
+        }
+
+
+
     }
 }
