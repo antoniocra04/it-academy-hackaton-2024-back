c17fd08308936599a829def1cea379c883ab87d0
Vladimir <vova-gorohov04@mail.ru>
2024-05-22 17:47:34 +0700

    Добавил поля Login и Password для User

--- InterestClubWebAPI/Models/User.cs
+++ InterestClubWebAPI/Models/User.cs
@@ -9,6 +9,10 @@ namespace InterestClubWebAPI.Models
 
         public string Surname { get; set; }
 
+        public string Login { get; set; }
+
+        public string Password { get; set; }
+
         public string? Fatherland { get; set; }
 
         public Role Role { get; set; }
--- InterestClubWebAPI/obj/Debug/net8.0/InterestClubWebAPI.AssemblyInfo.cs
+++ InterestClubWebAPI/obj/Debug/net8.0/InterestClubWebAPI.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InterestClubWebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a25bbb84b94ffa1f7b2a169f8649ea820997051")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+625e500cce8bdb756bf69fdf3a8326f8553fab9f")]
 [assembly: System.Reflection.AssemblyProductAttribute("InterestClubWebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InterestClubWebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
