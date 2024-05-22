14504444b4a4632b0562bde2e852fcaa7c72015d
Vladimir <vova-gorohov04@mail.ru>
2024-05-22 22:57:28 +0700

    Почистил мусор

--- InterestClubWebAPI/Controllers/UsersController.cs
+++ InterestClubWebAPI/Controllers/UsersController.cs
@@ -7,45 +7,32 @@ using System.Xml.Linq;
 
 namespace InterestClubWebAPI.Controllers
 {
-    public class UsersController : Controller
-    {
-        private readonly ApplicationContext dbContext;
-        public UsersController(ApplicationContext dbContext)
-        {
-            this.dbContext = dbContext;
-        }
+    public class UsersController : Controller    {
+            
         public IActionResult Index()
         {
             return View();
         }
+
         [HttpPost("singUp")]
-        public IActionResult SingUp(string login, string password, string confirmation_pasword)
+        public IActionResult SingUp(string login, string password)
         {
-            if (string.IsNullOrEmpty(login) || string.IsNullOrEmpty(password) || string.IsNullOrEmpty(confirmation_pasword))// защита от пустых строк
-                return NotFound();
-            //var user = dbContext.Users.Where(p => p.Login == login).ToList();
-
-            #region Тест 
-            // задел на допил после создания frontend части
-            if (dbContext.Users.Any(p => p.Login == login))
-            {
-                //вернуть сообщение что логин занят
-            }
-            if(confirmation_pasword == password)
-            {
-                // вернуть сообщение о не соответствии паролей
-            }
-            #endregion
-
-            if (!dbContext.Users.Any(p => p.Login == login ) && confirmation_pasword == password )// проверка свободного логина и соответствия поролей
-            {
-                //стоит дороботать логику сопостовления паролей и не занятость логина 
-                dbContext.Users.Add(new User { Login=login, Password= password});
-                dbContext.SaveChanges();
-                return Ok();
-            }
-            else
-                return BadRequest();
+            using (ApplicationContext db = new ApplicationContext())
+            {               
+                
+                if (db.Users.Any(p => p.Login == login))
+                {
+                    return BadRequest();
+                }
+                else
+                {
+                    //стоит дороботать логику сопостовления паролей и не занятость логина 
+                    db.Users.Add(new User { Login = login, Password = password });
+                    db.SaveChanges();
+                    return Ok();
+                }   
+            }  
+                
         }
 
     }
