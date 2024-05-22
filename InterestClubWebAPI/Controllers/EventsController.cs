5dd20ac72c4828b2662145734475356661f3bcbb
Стас Затеев <zateev.stanislav@gmail.com>
2024-05-22 13:01:23 +0700

    Added AddEvent

--- InterestClubWebAPI/Controllers/EventsController.cs
+++ InterestClubWebAPI/Controllers/EventsController.cs
@@ -1,12 +1,30 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿﻿using Microsoft.AspNetCore.Mvc;
 
 namespace InterestClubWebAPI.Controllers
 {
     public class EventsController : Controller
     {
+        private readonly ApplicationContext dbContext;
+        IWebHostEnvironment _appEnvironment;
         public IActionResult Index()
         {
             return View();
         }
+       
+        
+        [HttpPost("AddEvent")]
+        public async Task<ActionResult> AddEvent(Event events)
+        {
+            dbContext.Events.Add(events);
+            dbContext.SaveChanges();
+            return Ok(events);
+        }
+        
+        public EventsController(ApplicationContext dbContext, IWebHostEnvironment appEnvironment)
+        {
+            dbContext = dbContext;
+            _appEnvironment = appEnvironment;
+        }
+
     }
 }
