e8f0f59b59adba359fc1566a8a1360ffe1fcd9d9
Maxut <55204645+SphaleRud@users.noreply.github.com>
2024-05-23 16:45:47 +0700

    Добавил удаление клубов

--- InterestClubWebAPI/Controllers/ClubController.cs
+++ InterestClubWebAPI/Controllers/ClubController.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using InterestClubWebAPI.Context;
+using InterestClubWebAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Data.Entity;
+using System.Xml.Linq;
 
 namespace InterestClubWebAPI.Controllers
 {
@@ -18,6 +23,24 @@ namespace InterestClubWebAPI.Controllers
                     db.Club.Add(new Club { Title = title, Deksription = deskription });
                     db.SaveChanges();
                     return Ok();
+                }
+            }
+        }
+
+        [HttpPost("delClub")]
+        public IActionResult DelClub(string title)
+        {
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                if (db.Club.Any(club => club.Title == title))
+                {
+                    db.Club.Remove(db.Club.Where(club => club.Title == title).FirstOrDefault());
+                    db.SaveChanges();
+                    return Ok();
+                }
+                else
+                {                    
+                    return BadRequest();
                 }
             }
         }
