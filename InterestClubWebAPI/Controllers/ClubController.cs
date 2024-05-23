7c3072b9525a6d91f704b63c36f0a6ae51cb5439
OneHalfCat <59948456+OneHalfCat@users.noreply.github.com>
2024-05-23 16:32:21 +0700

    Update ClubController.cs

--- InterestClubWebAPI/Controllers/ClubController.cs
+++ InterestClubWebAPI/Controllers/ClubController.cs
@@ -4,6 +4,23 @@ namespace InterestClubWebAPI.Controllers
 {
     public class ClubController : Controller
     {
+        [HttpPost("CreateClub")]
+        public IActionResult SingUp(string title, string deskription)
+        {
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                if (db.Club.Any(Club => club.Titlte == title))
+                {
+                    return BadRequest();
+                }
+                else
+                {                    
+                    db.Club.Add(new Club { Title = title, Deksription = deskription });
+                    db.SaveChanges();
+                    return Ok();
+                }
+            }
+        }
         public IActionResult Index()
         {
             return View();
