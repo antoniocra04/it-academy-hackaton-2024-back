6187288fbc89df5dabb0f37ed129a95f7fad15dc
Vladimir <vova-gorohov04@mail.ru>
2024-05-23 21:43:14 +0700

    Завершил работу с CreateClub и DeleteClub

---
+++ InterestClubWebAPI/Migrations/20240523131516_AddClubEventUserRelationship.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InterestClubWebAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddClubEventUserRelationship : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Events_Clubs_ClubId",
+                table: "Events");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Users_Clubs_ClubId",
+                table: "Users");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Users_Events_EventId",
+                table: "Users");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Users_ClubId",
+                table: "Users");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Users_EventId",
+                table: "Users");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Events_ClubId",
+                table: "Events");
+
+            migrationBuilder.DropColumn(
+                name: "ClubId",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "EventId",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "ClubId",
+                table: "Events");
+
+            migrationBuilder.CreateTable(
+                name: "ClubEvents",
+                columns: table => new
+                {
+                    EventId = table.Column<Guid>(type: "uuid", nullable: false),
+                    ClubId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ClubEvents", x => new { x.ClubId, x.EventId });
+                    table.ForeignKey(
+                        name: "FK_ClubEvents_Clubs_ClubId",
+                        column: x => x.ClubId,
+                        principalTable: "Clubs",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ClubEvents_Events_EventId",
+                        column: x => x.EventId,
+                        principalTable: "Events",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "EventMembers",
+                columns: table => new
+                {
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    EventId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EventMembers", x => new { x.EventId, x.UserId });
+                    table.ForeignKey(
+                        name: "FK_EventMembers_Events_EventId",
+                        column: x => x.EventId,
+                        principalTable: "Events",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_EventMembers_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserClubs",
+                columns: table => new
+                {
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    ClubId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserClubs", x => new { x.UserId, x.ClubId });
+                    table.ForeignKey(
+                        name: "FK_UserClubs_Clubs_ClubId",
+                        column: x => x.ClubId,
+                        principalTable: "Clubs",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_UserClubs_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ClubEvents_EventId",
+                table: "ClubEvents",
+                column: "EventId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EventMembers_UserId",
+                table: "EventMembers",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserClubs_ClubId",
+                table: "UserClubs",
+                column: "ClubId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ClubEvents");
+
+            migrationBuilder.DropTable(
+                name: "EventMembers");
+
+            migrationBuilder.DropTable(
+                name: "UserClubs");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "ClubId",
+                table: "Users",
+                type: "uuid",
+                nullable: true);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "EventId",
+                table: "Users",
+                type: "uuid",
+                nullable: true);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "ClubId",
+                table: "Events",
+                type: "uuid",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_ClubId",
+                table: "Users",
+                column: "ClubId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_EventId",
+                table: "Users",
+                column: "EventId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Events_ClubId",
+                table: "Events",
+                column: "ClubId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Events_Clubs_ClubId",
+                table: "Events",
+                column: "ClubId",
+                principalTable: "Clubs",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Users_Clubs_ClubId",
+                table: "Users",
+                column: "ClubId",
+                principalTable: "Clubs",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Users_Events_EventId",
+                table: "Users",
+                column: "EventId",
+                principalTable: "Events",
+                principalColumn: "Id");
+        }
+    }
+}
