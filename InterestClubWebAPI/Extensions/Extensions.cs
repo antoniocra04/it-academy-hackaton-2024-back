fff6e22650c7b77bdb62a334b4132ed4daa70ad6
Vladimir <vova-gorohov04@mail.ru>
2024-05-26 15:52:02 +0700

    Правки и дополнения к роутам #24

--- InterestClubWebAPI/Extensions/Extensions.cs
+++ InterestClubWebAPI/Extensions/Extensions.cs
@@ -1,5 +1,7 @@
 ﻿using InterestClubWebAPI.Models.InterestClubWebAPI.DTOs;
 using InterestClubWebAPI.Models;
+using System;
+using Microsoft.Extensions.Logging;
 
 
 namespace InterestClubWebAPI.Extensions
@@ -15,9 +17,9 @@ namespace InterestClubWebAPI.Extensions
                 Description = club.Description,
                 FullDescription = club.FullDescription,
                 CreatorClubID = club.CreatorClubID,
+                CountMembers = club.Users.Count(),
                 Users = club.Users.Select(u => u.ToDTO()).ToList(),
-                Events = club.Events.Select(e => e.ToDTO()).ToList(),
-                CountMembers = club.Users.Count()
+                Events = club.Events.Select(e => e.ToDTO()).ToList(),                
             };
         }
     }
@@ -26,16 +28,29 @@ namespace InterestClubWebAPI.Extensions
     {
         public static UserDTO ToDTO(this User user)
         {
-            return new UserDTO
+            return  new UserDTO
             {
                 Id = user.Id,
                 Name = user.Name,
                 Surname = user.Surname,
-                Login = user.Login, 
+                Login = user.Login,
                 Password = user.Password,
                 Fatherland = user.Fatherland,
-                Role = user.Role                
+                Role = user.Role,
+                ClubsId = user.Clubs.Select(c => c.Id.ToString()).ToList(),
+                EventsId = user.Events.Select(e => e.Id.ToString()).ToList(),
             };
+            
+            
+            //foreach (var club in user.Clubs)
+            //{
+            //    userDTO.ClubsId.Add(club.Id.ToString());
+            //}
+            //foreach (var ev in user.Events)
+            //{
+            //    userDTO.EventsId.Add(ev.Id.ToString());
+            //}
+            //return userDTO;
         }
     }
 
@@ -51,7 +66,9 @@ namespace InterestClubWebAPI.Extensions
                 CreatorEventID = ev.CreatorEventID,
                 ClubID = ev.ClubID,
                 EventDate = ev.EventDate,
-                Members = ev.Members.Select(m => m.ToDTO()).ToList()
+                MembersCount = ev.Members.Count(),
+                Members = ev.Members.Select(m => m.ToDTO()).ToList(),
+                
             };
         }
     }
