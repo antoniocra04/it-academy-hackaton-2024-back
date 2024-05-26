08472628be1e7058f876536f0ff0725edc3564fb
Vladimir <vova-gorohov04@mail.ru>
2024-05-26 18:32:17 +0700

    Исправление Недочетов

---
+++ InterestClubWebAPI/Models/File.cs
@@ -0,0 +1,9 @@
+﻿namespace InterestClubWebAPI.Models
+{
+    public class File
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Path { get; set; }
+    }
+}
